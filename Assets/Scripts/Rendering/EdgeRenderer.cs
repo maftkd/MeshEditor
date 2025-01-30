696461788d006a6fa6c9ee1e4d71e61320ef6811
Michael <michael.feldman09@gmail.com>
2025-01-29 19:52:07 -0500

    Edges shaded with different color when highlighted

--- Assets/Scripts/Rendering/EdgeRenderer.cs
+++ Assets/Scripts/Rendering/EdgeRenderer.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Edge = SelectionManager.Edge;
 
 public class EdgeRenderer : MonoBehaviour
 {
+    public Shader shader;
     private Material _mat;
     public MyMesh mesh;
     // Start is called before the first frame update
@@ -17,20 +15,31 @@ public class EdgeRenderer : MonoBehaviour
     {
         if (_mat == null)
         {
-            Shader shader = Shader.Find("Unlit/Color");
             _mat = new Material(shader);
             _mat.color = Color.black;
         }
 
         _mat.SetPass(0);
+        
         GL.PushMatrix();
+        
         GL.Begin(GL.LINES);
         foreach (Edge e in mesh.edges)
         {
+            if (e.a.selected && e.b.selected)
+            {
+                GL.Color(Color.green);
+            }
+            else
+            {
+                GL.Color(Color.black);
+                
+            }
             GL.Vertex(e.a.position);
             GL.Vertex(e.b.position);
         }
         GL.End();
+        
         GL.PopMatrix();
     }
 }
---
+++ Assets/Shaders/Edge.shader
@@ -0,0 +1,46 @@
+Shader "Unlit/Edge"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float3 color : COLOR;
+            };
+
+            struct v2f
+            {
+                float3 color : COLOR;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.color = v.color;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return float4(i.color, 1);
+            }
+            ENDCG
+        }
+    }
+}
