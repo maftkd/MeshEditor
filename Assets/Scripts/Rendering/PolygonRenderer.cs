339664a9c89390507bf747783fc8306590d44f41
Michael <michael.feldman09@gmail.com>
2025-02-18 14:59:22 -0500

    Differentiate face color when selected in face mode
    Again just copying a Blender feature. Selected faces are colored
    differently when in face selection mode or otherwise.

--- Assets/Scripts/Rendering/PolygonRenderer.cs
+++ Assets/Scripts/Rendering/PolygonRenderer.cs
@@ -30,6 +30,9 @@ public class PolygonRenderer : MonoBehaviour
 
         _mat.SetPass(0);
         
+        Color selectedColor = selectionManager.selectionMode == SelectionManager.SelectionMode.Face ? 
+            new Color(0.75f, 1.5f, 0.75f) : new Color(1f, 1.15f, 1f);
+        
         GL.PushMatrix();
         
         GL.Begin(GL.TRIANGLES);
@@ -38,7 +41,7 @@ public class PolygonRenderer : MonoBehaviour
             Vector3 viewNorm = _cam.worldToCameraMatrix * new Vector4(poly.normal.x, poly.normal.y, poly.normal.z, 0.0f);
             float dt = -Vector3.Dot(viewNorm, lightDirViewSpace.normalized) * 0.5f + 0.5f;
             float lighting = Mathf.Lerp(0.3f, 0.6f, dt);
-            Color baseCol = poly.selected ? new Color(0.75f, 1.5f, 0.75f) : Color.white;
+            Color baseCol = poly.selected ? selectedColor : Color.white;
             GL.Color(baseCol * lighting);
             foreach (Vertex v in poly.tris)
             {
