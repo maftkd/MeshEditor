52010e0346c47d5bcf09a79e87e3abaeffbadb6a
Michael <michael.feldman09@gmail.com>
2025-02-18 15:45:12 -0500

    Start with a cube mesh
    Encapsulated the edge, loop, and poly creation for quad faces of a cube,
    and create a basic quad. Not sure if normals are correct. In fact I
    think they are wrong, but we can worry about that way later.
    
    This should help test more complex deletion cases, but we shall see.
    
    Also this is kind of cool now that we actually have a 3d mesh

--- Assets/Scripts/MyMesh.cs
+++ Assets/Scripts/MyMesh.cs
@@ -15,28 +15,43 @@ public class MyMesh : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //tmp initialize to a quad
-        vertices.Add(new Vertex(new Vector3(-0.5f, -0.5f, 0)));
-        vertices.Add(new Vertex(new Vector3(0.5f, -0.5f, 0)));
-        vertices.Add(new Vertex(new Vector3(0.5f, 0.5f, 0)));
-        vertices.Add(new Vertex(new Vector3(-0.5f, 0.5f, 0)));
+        //tmp initialize to a cube
+        vertices.Add(new Vertex(new Vector3(-0.5f, -0.5f, -0.5f))); //front bottom left
+        vertices.Add(new Vertex(new Vector3(0.5f, -0.5f, -0.5f))); //front bottom right
+        vertices.Add(new Vertex(new Vector3(0.5f, 0.5f, -0.5f))); //front top right
+        vertices.Add(new Vertex(new Vector3(-0.5f, 0.5f, -0.5f))); //front top left
         
+        vertices.Add(new Vertex(new Vector3(-0.5f, -0.5f, 0.5f))); //back bottom left
+        vertices.Add(new Vertex(new Vector3(0.5f, -0.5f, 0.5f))); //back bottom right
+        vertices.Add(new Vertex(new Vector3(0.5f, 0.5f, 0.5f))); //back top right
+        vertices.Add(new Vertex(new Vector3(-0.5f, 0.5f, 0.5f))); //back top left
+        
+        CreateFace(vertices[0], vertices[1], vertices[2], vertices[3]); //front face
+        CreateFace(vertices[1], vertices[5], vertices[6], vertices[2]); //right face
+        CreateFace(vertices[5], vertices[4], vertices[7], vertices[6]); //back face
+        CreateFace(vertices[4], vertices[0], vertices[3], vertices[7]); //left face
+        CreateFace(vertices[3], vertices[2], vertices[6], vertices[7]); //top face
+        CreateFace(vertices[4], vertices[5], vertices[1], vertices[0]); //bottom face
+    }
+
+    void CreateFace(Vertex a, Vertex b, Vertex c, Vertex d)
+    {
         //edges
-        edges.Add(new Edge(vertices[0], vertices[1]));
-        edges.Add(new Edge(vertices[1], vertices[2]));
-        edges.Add(new Edge(vertices[2], vertices[3]));
-        edges.Add(new Edge(vertices[3], vertices[0]));
+        edges.Add(new Edge(a, b));
+        edges.Add(new Edge(b, c));
+        edges.Add(new Edge(c, d));
+        edges.Add(new Edge(d, a));
         
         //loops
-        loops.Add(new Loop(vertices[0], edges[0]));
-        loops.Add(new Loop(vertices[1], edges[1]));
-        loops.Add(new Loop(vertices[2], edges[2]));
-        loops.Add(new Loop(vertices[3], edges[3]));
+        loops.Add(new Loop(a, edges[^4]));
+        loops.Add(new Loop(b, edges[^3]));
+        loops.Add(new Loop(c, edges[^2]));
+        loops.Add(new Loop(d, edges[^1]));
         
-        //polygons
-        polygons.Add(new Polygon(0, 4));
-        PolygonHelper.CalculateNormal(polygons[0], this);
-        PolygonHelper.Triangulate(polygons[0], this);
+        //polygon
+        polygons.Add(new Polygon(loops.Count - 4, 4));
+        PolygonHelper.CalculateNormal(polygons[^1], this);
+        PolygonHelper.Triangulate(polygons[^1], this);
     }
 
     // Update is called once per frame
